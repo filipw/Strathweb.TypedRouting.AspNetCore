766746e5d532a8ea146f72a604cacbaec516bda8
filipw <wojcieszyn@gmail.com>
2016-05-27 23:31:25 +0200

    added demo

---
+++ Strathweb.TypedRouting.AspNetCore/sample/Demo/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class ValuesController : Controller
+    {
+        //[HttpGet("api/values")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public void Post([FromBody]string value)
+        {
+        }
+
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Strathweb.TypedRouting.AspNetCore/sample/Demo/Startup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Strathweb.TypedRouting.AspNetCore;
+using Demo.Controllers;
+
+namespace Demo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {}
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(opt =>
+            {
+                opt.EnableTypedRouting();
+                opt.GetRoute("api/values", c => c.Action<ValuesController>(x => x.Get()));
+                opt.GetRoute("api/values/{id}", c => c.Action<ValuesController>(x => x.Get(Param<int>.Any)));
+                opt.PostRoute("api/values", c => c.Action<ValuesController>(x => x.Post(Param<string>.Any)));
+                opt.PutRoute("api/values/{id}", c => c.Action<ValuesController>(x => x.Put(Param<int>.Any, Param<string>.Any)));
+                opt.DeleteRoute("api/values/{id}", c => c.Action<ValuesController>(x => x.Delete(Param<int>.Any)));
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddDebug();
+            app.UseMvc();
+        }
+    }
+}
